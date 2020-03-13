28e1cb401e39da4d8b5727e59d8c7365209dfb62
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-12 22:50:17 -0300

    added more steps

--- thingsboard-cli/cli.py
+++ thingsboard-cli/cli.py
@@ -61,3 +61,106 @@
 #   "title": "string",
 #   "zip": "string"
 # }
+
+# 3 - criar um user dentro do customer -> /api/user{?sendActivationMail,entityGroupId}
+# params: {
+#   "additionalInfo": "string",
+#   "authority": "SYS_ADMIN",
+#   "createdTime": 0,
+#   "customerId": {
+#     "id": "string"
+#   },
+#   "email": "string",
+#   "firstName": "string",
+#   "id": {
+#     "id": "string"
+#   },
+#   "lastName": "string",
+#   "name": "string",
+#   "ownerId": {
+#     "entityType": "TENANT",
+#     "id": "string"
+#   },
+#   "tenantId": {
+#     "id": "string"
+#   }
+# }
+
+# 4 - dar permissão para esse usuario -> /api/permissions/allowedPermissions
+# params: {
+#   "allowedForGroupOwnerOnlyGroupOperations": [
+#     "ALL"
+#   ],
+#   "allowedForGroupOwnerOnlyOperations": [
+#     "ALL"
+#   ],
+#   "allowedForGroupRoleOperations": [
+#     "ALL"
+#   ],
+#   "allowedResources": [
+#     "ALL"
+#   ],
+#   "operationsByResource": {},
+#   "userOwnerId": {
+#     "entityType": "TENANT",
+#     "id": "string"
+#   },
+#   "userPermissions": {
+#     "genericPermissions": {},
+#     "groupPermissions": {},
+#     "readGroupPermissions": {}
+#   }
+# }
+
+# 5 - criar o device -> /api/device{?accessToken,entityGroupId}
+# params: {
+#   "additionalInfo": "string",
+#   "createdTime": 0,
+#   "customerId": {
+#     "id": "string"
+#   },
+#   "id": {
+#     "id": "string"
+#   },
+#   "label": "string",
+#   "name": "string",
+#   "ownerId": {
+#     "entityType": "TENANT",
+#     "id": "string"
+#   },
+#   "tenantId": {
+#     "id": "string"
+#   },
+#   "type": "string"
+# }
+
+# 6 - criar dashboard -> /api/dashboard{?entityGroupId}
+# params: {
+#   "assignedCustomers": [
+#     {
+#       "customerId": {
+#         "id": "string"
+#       },
+#       "public": true,
+#       "title": "string",
+#       "isPublic": true
+#     }
+#   ],
+#   "configuration": "string",
+#   "createdTime": 0,
+#   "customerId": {
+#     "id": "string"
+#   },
+#   "id": {
+#     "id": "string"
+#   },
+#   "name": "string",
+#   "ownerId": {
+#     "entityType": "TENANT",
+#     "id": "string"
+#   },
+#   "tenantId": {
+#     "id": "string"
+#   },
+#   "title": "string"
+# }
